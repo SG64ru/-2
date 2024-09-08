622a5223ff9fc82d539aa2186723a2885262272c
SG64ru <sergg64@gmail.com>
2024-09-09 00:00:51 +0500

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,7 @@
+example = "Крышечка" # задание 1
+print (example)
+print (example [0]) # задание 2
+print (example [-1]) # задание 3
+print (example [4:]) # задание 4
+print (example [::-1]) # задание 5
+print (example [1::2]) # задание 6
